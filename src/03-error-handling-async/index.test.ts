5ed0df81237fb67fc88701b1861c1b9577421f9f
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-27 23:30:16 +0300

    feat: error-handling-tests added

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,63 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const strTest = 'enter value';
+    const data = await resolveValue(strTest);
+    expect(data).toBe(strTest);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    const errorMessage = 'enter value';
+    try {
+      throwError(errorMessage);
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toBe(errorMessage);
+      }
+    }
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    const errorMessage = 'Oops!';
+    try {
+      throwError();
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toBe(errorMessage);
+      }
+    }
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    const errorMessage = 'This is my awesome custom error!';
+    try {
+      throwCustomError();
+    } catch (error) {
+      if (error instanceof MyAwesomeError) {
+        expect(error.message).toBe(errorMessage);
+      }
+    }
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    const errorMessage = 'This is my awesome custom error!';
+    await expect(rejectCustomError())
+      .rejects.toBeInstanceOf(MyAwesomeError)
+      .catch((err) => {
+        expect(err.message).toBe(errorMessage);
+      });
   });
 });
